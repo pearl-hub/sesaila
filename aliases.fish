820282311c3eb4ee203e478adb736355eabf3b37
Filippo Squillace <squillace.filippo@gmail.com>
2020-12-18 23:29:24 +0100

    Use bpytop as top command

--- aliases.fish
+++ aliases.fish
@@ -3,6 +3,8 @@ alias e="eval $EDITOR"
 
 # If the system has htop use it!
 [ -e /usr/bin/htop ]; and alias top="/usr/bin/htop"
+# If the system has bpytop even better!
+[ -e /usr/bin/bpytop ]; and alias top="/usr/bin/bpytop"
 
 # Only the newer version of ls supports the option --group-directories-first
 ls --group-directories-first > /dev/null ^ /dev/null
--- aliases.sh
+++ aliases.sh
@@ -7,6 +7,8 @@ alias e="$EDITOR"
 
 # If the system has htop use it!
 [ -e /usr/bin/htop ] && alias top="/usr/bin/htop"
+# If the system has bpytop even better!
+[ -e /usr/bin/bpytop ] && alias top="/usr/bin/bpytop"
 
 # Only the newer version of ls support the option --group-directories-first
 if ls --group-directories-first --color=auto &> /dev/null
